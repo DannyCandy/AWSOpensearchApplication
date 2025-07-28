f2e16a5ca7528b41e9b1dbfe199607bd01e776cf
NguyenDan <dankbzzz01@gmail.com>
2025-07-28 17:15:24 +0700

    publish branch

--- backend/src/routes/search.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { searchWithQuery } from '../controller/search.controller.js';
-
-const router = express.Router();
-
-// Search movies endpoint
-router.get('/movies', searchWithQuery);
-
-export default router;
--- frontend/src/lib/axios.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const axiosInstance = axios.create({
-	baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000" : import.meta.env.VITE_API_CLOUDFRONT_URL,
-	headers: {
-		"Content-Type": "application/json",
-	}
-});
